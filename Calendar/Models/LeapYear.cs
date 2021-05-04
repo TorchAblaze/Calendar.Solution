ecc6a50e6e265522800804a3f9b673c4733b2f91
TorchAblaze <tiffanygreathead@gmail.com>
2021-05-03 21:34:48 -0700

    Add first test
    Co-authored-by: Patrick Lee <patricklee1138@gmail.com>
    Co-authored-by: Cristina Plesa <cristinaplesa8@gmail.com>

--- Calendar/Models/LeapYear.cs
+++ Calendar/Models/LeapYear.cs
@@ -4,18 +4,8 @@ namespace Calendar
   {
     public bool IsLeapYear(int year)
     {
-      if (year % 400 == 0)
-      {
-        return true;
-      }
-      else if (year % 100 == 0)
-      {
-        return false;
-      }
-      else
-      {
-          return year % 4 == 0;
-      }
+      // logic will go here
+      return false;
     }
   }
 }
